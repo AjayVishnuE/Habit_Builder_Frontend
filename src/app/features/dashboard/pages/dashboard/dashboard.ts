323f7d5f528af78b11c3ce3e82a2567d742a3e2a
AjayVishnuE <ajayvishnu121@gmail.com>
2026-07-20 23:08:51 +0530

    dashboard development

--- src/app/features/dashboard/pages/dashboard/dashboard.ts
+++ src/app/features/dashboard/pages/dashboard/dashboard.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+
+import { HabitService } from '../../../../core/services/habit.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,4 +8,29 @@ import { Component } from '@angular/core';
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.sass',
 })
-export class Dashboard {}
+export class Dashboard {
+  public totalHabits = 0;
+  public completedToday = 0;
+  public currentStreak = 0;
+
+  private habitService = inject(HabitService);
+
+  ngOnInit() {
+    this.loadDashboard();
+  }
+
+  loadDashboard() {
+    this.habitService.getHabits().subscribe({
+      next: (habits) => {
+        this.totalHabits = habits.length;
+        const today = new Date().toDateString();
+        this.completedToday = habits.filter(habit =>
+          habit.completedDates.some(date =>
+            new Date(date).toDateString() === today
+          )
+        ).length;
+      }
+    });
+  }
+
+}
