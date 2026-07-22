ff764881dbaeb5200223e4284a95b7efd799b008
AjayVishnuE <ajayvishnu121@gmail.com>
2026-07-22 23:50:52 +0530

    dashboard statistics

--- src/app/features/dashboard/pages/dashboard/dashboard.ts
+++ src/app/features/dashboard/pages/dashboard/dashboard.ts
@@ -1,14 +1,17 @@
 import { Component, inject, ChangeDetectorRef } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { firstValueFrom } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { BaseChartDirective } from 'ng2-charts';
+import { ChartConfiguration, ChartType } from 'chart.js';
 
+import { Habit } from '../../../../core/models/habit.model';
 import { HabitService } from '../../../../core/services/habit.service';
 import { StatCard } from '../../components/stat-card/stat-card';
 
 @Component({
   selector: 'app-dashboard',
   imports: [
-    StatCard, CommonModule
+    StatCard, CommonModule,  BaseChartDirective,
   ],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.sass',
@@ -17,18 +20,21 @@ export class Dashboard {
   public totalHabits = 0;
   public completedToday = 0;
   public currentStreak = 0;
+  public barChartData:any;
+  habits: Habit[] = [];
 
   private habitService = inject(HabitService);
   private cdr = inject(ChangeDetectorRef);
+  public barChartType: ChartType = 'bar';
 
   async ngOnInit(): Promise<void> {
     await this.loadDashboard();
   }
 
-async loadDashboard(): Promise<void> {
+  async loadDashboard(): Promise<void> {
     try {
       const habits = await firstValueFrom(this.habitService.getHabits());
-
+      this.habits = habits;
       this.totalHabits = habits.length;
 
       const today = new Date().toDateString();
@@ -38,11 +44,56 @@ async loadDashboard(): Promise<void> {
           new Date(date).toDateString() === today
         )
       ).length;
+      await this.loadWeeklyChart();
+
       this.cdr.detectChanges();
 
     } catch (error) {
       console.error('Error loading dashboard stats:', error);
     }
   }
+
+  async loadWeeklyChart() {
+    const labels: string[] = [];
+    const values: number[] = [];
+    for (let i = 6; i >= 0; i--) {
+      const day = new Date();
+      day.setDate(day.getDate() - i);
+      labels.push(
+        day.toLocaleDateString('en-US', { weekday: 'short' })
+      );
+      let count = 0;
+      this.habits.forEach(habit => {
+        habit.completedDates.forEach(date => {
+          if (
+            new Date(date).toDateString() ===
+            day.toDateString()
+          ) {
+            count++;
+          }
+
+        });
+      });
+      values.push(count);
+    }
+    this.barChartData = {
+      labels,
+      datasets: [
+        {
+          label: 'Completed Habits',
+          data: values
+        }
+      ]
+    };
+  }
+
+  public barChartOptions: ChartConfiguration<'bar'>['options'] = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: true
+      }
+    }
+  };
 }
 
