0ddcf96c71c668338e093099aa379de2a7c80d47
AjayVishnuE <ajayvishnu121@gmail.com>
2026-07-25 22:30:19 +0530

    habits frequency made monthly & weekly

--- src/app/features/habits/components/habit-card/habit-card.ts
+++ src/app/features/habits/components/habit-card/habit-card.ts
@@ -11,12 +11,39 @@ import { Habit } from '../../../../core/models/habit.model';
   styleUrl: './habit-card.scss'
 })
 export class HabitCard {
-  isCompletedToday(): boolean {
-    const today = new Date().toDateString();
-    return this.habit.completedDates.some(
-      date => new Date(date).toDateString() === today
+  isCompleted(): boolean {
+    if (!this.habit.completedDates.length) {
+      return false;
+    }
+    const lastCompleted = new Date(
+      this.habit.completedDates[this.habit.completedDates.length - 1]
     );
+    const today = new Date();
+    switch (this.habit.frequency) {
+      case 'Daily':
+        return lastCompleted.toDateString() === today.toDateString();
+      case 'Weekly':
+        return this.isSameWeek(lastCompleted, today);
+      case 'Monthly':
+        return (
+            lastCompleted.getMonth() === today.getMonth() &&
+            lastCompleted.getFullYear() === today.getFullYear()
+        );
+      default:
+        return false;
+    }
   }
+
+  private isSameWeek(date1: Date, date2: Date): boolean {
+    const first = new Date(date1);
+    const second = new Date(date2);
+    first.setHours(0,0,0,0);
+    second.setHours(0,0,0,0);
+    const firstWeek = Math.floor(first.getTime() / (7 * 24 * 60 * 60 * 1000));
+    const secondWeek = Math.floor(second.getTime() / (7 * 24 * 60 * 60 * 1000));
+    return firstWeek === secondWeek;
+  }
+
   @Input({ required: true })
   habit!: Habit;
 
