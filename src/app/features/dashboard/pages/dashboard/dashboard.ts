e7c47b1f3f7b3f0776cf007a02a3ed54ab3ade7a
AjayVishnuE <ajayvishnu121@gmail.com>
2026-07-27 22:32:07 +0530

    Completion Percentage in Dashboard

--- src/app/features/dashboard/pages/dashboard/dashboard.ts
+++ src/app/features/dashboard/pages/dashboard/dashboard.ts
@@ -2,7 +2,9 @@ import { Component, inject, ChangeDetectorRef } from '@angular/core';
 import { firstValueFrom } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { BaseChartDirective } from 'ng2-charts';
+import { MatCardModule } from '@angular/material/card';
 import { ChartConfiguration, ChartType } from 'chart.js';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 import { Habit } from '../../../../core/models/habit.model';
 import { HabitService } from '../../../../core/services/habit.service';
@@ -12,17 +14,20 @@ import { calculateLongestStreak } from '../../../../core/utils/streak.util';
 @Component({
   selector: 'app-dashboard',
   imports: [
-    StatCard, CommonModule,  BaseChartDirective,
+    StatCard, CommonModule,  BaseChartDirective, MatCardModule, MatProgressBarModule
   ],
   templateUrl: './dashboard.html',
-  styleUrl: './dashboard.sass',
+  styleUrl: './dashboard.scss',
 })
 export class Dashboard {
   public totalHabits = 0;
   public completedToday = 0;
   public currentStreak = 0;
   public barChartData:any;
-  habits: Habit[] = [];
+  public dailyPercentage = 0;
+  public weeklyPercentage = 0;
+  public monthlyPercentage = 0;
+  public habits: Habit[] = [];
 
   private habitService = inject(HabitService);
   private cdr = inject(ChangeDetectorRef);
@@ -30,6 +35,7 @@ export class Dashboard {
 
   async ngOnInit(): Promise<void> {
     await this.loadDashboard();
+    await this.calculateCompletionPercentage();
   }
 
   async loadDashboard(): Promise<void> {
@@ -50,9 +56,7 @@ export class Dashboard {
         )
       ).length;
       await this.loadWeeklyChart();
-
       this.cdr.detectChanges();
-
     } catch (error) {
       console.error('Error loading dashboard stats:', error);
     }
@@ -100,5 +104,72 @@ export class Dashboard {
       }
     }
   };
+
+  async calculateCompletionPercentage() {
+    const today = new Date();
+    const dailyHabits = this.habits.filter(h => h.frequency === 'Daily');
+    const weeklyHabits = this.habits.filter(h => h.frequency === 'Weekly');
+    const monthlyHabits = this.habits.filter(h => h.frequency === 'Monthly');
+    const completedDaily = dailyHabits.filter(h =>
+      this.isCompleted(h, today)
+    ).length;
+    const completedWeekly = weeklyHabits.filter(h =>
+      this.isCompleted(h, today)
+    ).length;
+    const completedMonthly = monthlyHabits.filter(h =>
+      this.isCompleted(h, today)
+    ).length;
+    this.dailyPercentage =
+      dailyHabits.length
+        ? Math.round((completedDaily / dailyHabits.length) * 100)
+        : 0;
+    this.weeklyPercentage =
+      weeklyHabits.length
+        ? Math.round((completedWeekly / weeklyHabits.length) * 100)
+        : 0;
+    this.monthlyPercentage =
+      monthlyHabits.length
+        ? Math.round((completedMonthly / monthlyHabits.length) * 100)
+        : 0;
+    this.cdr.detectChanges();
+  }
+
+  isCompleted(habit: Habit, today: Date): boolean {
+    if (!habit.completedDates.length) {
+      return false;
+    }
+    const lastCompleted = new Date(
+      habit.completedDates[habit.completedDates.length - 1]
+    );
+    switch (habit.frequency) {
+      case 'Daily':
+        return lastCompleted.toDateString() === today.toDateString();
+      case 'Weekly':
+        return this.isSameWeek(lastCompleted, today);
+      case 'Monthly':
+        return (
+          lastCompleted.getMonth() === today.getMonth() &&
+          lastCompleted.getFullYear() === today.getFullYear()
+        );
+      default:
+        return false;
+    }
+  }
+
+  private isSameWeek(date1: Date, date2: Date): boolean {
+    const startOfWeek = (date: Date) => {
+      const d = new Date(date);
+      const day = d.getDay();
+      const diff = day === 0 ? -6 : 1 - day;
+      d.setDate(d.getDate() + diff);
+      d.setHours(0, 0, 0, 0);
+      return d;
+    };
+    return (
+      startOfWeek(date1).getTime() ===
+      startOfWeek(date2).getTime()
+    );
+
+  }
 }
 
