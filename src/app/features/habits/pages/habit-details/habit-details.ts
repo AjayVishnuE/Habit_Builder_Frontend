687a0fae378e7f1f29cd799f4358f7cb9c63d120
AjayVishnuE <ajayvishnu121@gmail.com>
2026-08-01 17:17:45 +0530

    habit details page implementation 1

--- src/app/features/habits/pages/habit-details/habit-details.ts
+++ src/app/features/habits/pages/habit-details/habit-details.ts
@@ -1,12 +1,17 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { HabitService } from '../../../../core/services/habit.service';
+import { Component, OnInit, inject, ChangeDetectorRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+
 import { Habit } from '../../../../core/models/habit.model';
+import { HabitService } from '../../../../core/services/habit.service';
 
 @Component({
     selector: 'app-habit-details',
     standalone: true,
-    imports: [],
+    imports: [
+        CommonModule,
+        RouterModule
+    ],
     templateUrl: './habit-details.html',
     styleUrl: './habit-details.scss'
 })
@@ -14,29 +19,21 @@ export class HabitDetails implements OnInit {
 
     private route = inject(ActivatedRoute);
     private habitService = inject(HabitService);
+    private cdr = inject(ChangeDetectorRef);
 
     habit?: Habit;
 
-    ngOnInit(): void {
-
+    ngOnInit() {
         const id = this.route.snapshot.paramMap.get('id');
-
         if (!id) {
             return;
         }
-
         this.habitService.getHabitById(id).subscribe({
-
             next: habit => {
                 this.habit = habit;
+                this.cdr.detectChanges();
             },
-
-            error: err => {
-                console.error(err);
-            }
-
+            error: err => console.error(err)
         });
-
     }
-
 }
