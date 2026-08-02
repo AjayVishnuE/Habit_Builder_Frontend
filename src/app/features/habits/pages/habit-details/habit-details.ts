ca1c1d0e797dc869c315ed0ddea5c34d861d7bdc
AjayVishnuE <ajayvishnu121@gmail.com>
2026-08-02 17:56:23 +0530

    habit details page implementation 3

--- src/app/features/habits/pages/habit-details/habit-details.ts
+++ src/app/features/habits/pages/habit-details/habit-details.ts
@@ -20,8 +20,12 @@ export class HabitDetails implements OnInit {
     private route = inject(ActivatedRoute);
     private habitService = inject(HabitService);
     private cdr = inject(ChangeDetectorRef);
-
-    habit?: Habit;
+    public totalCompletions = 0;
+    public completionRate = 0;
+    public averageDuration = 0;
+    public mostCommonMood = '-';
+    public lastCompleted = '';
+    public habit?: Habit;
 
     ngOnInit() {
         const id = this.route.snapshot.paramMap.get('id');
@@ -31,9 +35,58 @@ export class HabitDetails implements OnInit {
         this.habitService.getHabitById(id).subscribe({
             next: habit => {
                 this.habit = habit;
+                this.calculateInsights();
                 this.cdr.detectChanges();
             },
             error: err => console.error(err)
         });
+    }
+
+    calculateInsights() {
+        if (!this.habit) {
+            return;
+        }
+        const history = this.habit.completedHistory;
+        this.totalCompletions = history.length;
+        if (!history.length) {
+            return;
+        }
+        this.lastCompleted =
+            history[history.length - 1].completedAt;
+        const totalDuration = history.reduce( (sum, entry) => sum + (entry.duration || 0), 0 );
+        this.averageDuration = Math.round(totalDuration / history.length);
+        const moods: Record<string, number> = {};
+        history.forEach(entry => {
+            moods[entry.mood] =
+            (moods[entry.mood] || 0) + 1;
+        });
+        this.mostCommonMood = Object.keys(moods).reduce((a, b) => moods[a] > moods[b] ? a : b );
+        this.completionRate = this.calculateCompletionRate();
+    }
+
+    private calculateCompletionRate(): number {
+    if (!this.habit) {
+        return 0;
+    }
+    const created = new Date(this.habit.createdAt);
+    const today = new Date();
+    let expected = 0;
+    switch (this.habit.frequency) {
+        case 'Daily':
+        expected = Math.floor( (today.getTime() - created.getTime()) / (1000 * 60 * 60 * 24) ) + 1;
+        break;
+        case 'Weekly':
+        expected = Math.ceil( (today.getTime() - created.getTime()) / (1000 * 60 * 60 * 24 * 7) );
+        break;
+        case 'Monthly':
+        expected = (today.getFullYear() - created.getFullYear()) * 12 + (today.getMonth() - created.getMonth()) + 1;
+        break;
+    }
+    if (expected <= 0) {
+        return 100;
+    }
+    return Math.round(
+        this.totalCompletions * 100 / expected
+    );
     }
 }
