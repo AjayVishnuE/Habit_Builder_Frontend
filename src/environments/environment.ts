e702e24c36e3ce1e61b68ec9935b1ebe371ec5aa
AjayVishnuE <ajayvishnu121@gmail.com>
2026-08-09 22:14:00 +0530

    prod api mismatch

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false, //https://habit-builder-backend-u1qh.onrender.com/api
-  apiUrl: 'hhttps://habit-builder-backend-u1qh.onrender.com/api' //http://localhost:5000/api  
+  apiUrl: 'https://habit-builder-backend-u1qh.onrender.com/api' //http://localhost:5000/api  
 };
